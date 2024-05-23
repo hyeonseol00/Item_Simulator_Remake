e84cf2fe78f4f77581015d94cc6a8a2513ab1d7a
hyeonseol00 <hyeonseol22@naver.com>
2024-05-23 19:59:00 +0900

    🎉 add : 프로젝트 시작

---
+++ source/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import expressSession from "express-session";
+import dotEnv from "dotenv";
+import expressMySQLSession from "express-mysql-session";
+
+dotEnv.config();
+
+const app = express();
+const PORT = 3018;
+
+const MySQLStore = expressMySQLSession(expressSession);
+const sessionStore = new MySQLStore({
+	user: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	database: process.env.DATABASE_NAME,
+	expiration: 1000 * 60 * 60 * 1,
+	createDatabaseTable: true,
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(expressSession(
+	{
+		secret: process.env.SESSION_SECRET_KEY,
+		resave: false,
+		saveUninitialized: false,
+		store: sessionStore,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 1,
+		},
+	}
+));
+app.use('/api', []);
+
+app.listen(PORT, () =>
+{
+	console.log(PORT, '포트로 서버가 열렸어요!');
+});
