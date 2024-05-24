2925fead47da3b37011311f7e0e7937f6a2007d5
hyeonseol00 <hyeonseol22@naver.com>
2024-05-24 12:16:29 +0900

    ✨ update : 캐릭터 상세 조회 API 추가

--- source/routes/characters.router.js
+++ source/routes/characters.router.js
@@ -35,14 +35,46 @@ router.post('/character', authMiddleware, async (req, res, next) =>
 /** 캐릭터 삭제 API **/
 router.delete('/character', authMiddleware, async (req, res, next) =>
 {
+	const { id } = req.account;
 	const { name } = req.body;
 	const character = await prisma.characters.findFirst({ where: { name } });
+
 	if (!character)
 		return res.status(404).json({ errorMessage: '존재하지 않는 캐릭터입니다.' });
+	else if (id != character.accountId)
+		return res.status(400).json({ errorMessage: '다른 계정의 캐릭터는 삭제할 수 없습니다!' });
 
 	await prisma.characters.delete({ where: { name } });
 
 	return res.status(200).json({ message: "캐릭터 삭제 완료" });
+});
+
+/** 캐릭터 상세 조회 API **/
+router.get('/character', authMiddleware, async (req, res, next) =>
+{
+	const { id } = req.account;
+	const { name } = req.body;
+	const character = await prisma.characters.findFirst({ where: { name } });
+
+	if (!character)
+		return res.status(404).json({ errorMessage: '존재하지 않는 캐릭터입니다.' });
+
+	let message;
+	if (id == character.accountId)
+		message = {
+			name: character.name,
+			health: character.health,
+			power: character.power,
+			money: character.money
+		};
+	else
+		message = {
+			name: character.name,
+			health: character.health,
+			power: character.power
+		};
+
+	return res.status(200).json({ message });
 });
 
 
