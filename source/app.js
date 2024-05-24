1a8b23134df24b232a52bcaada02d20929032f20
hyeonseol00 <hyeonseol22@naver.com>
2024-05-24 21:06:34 +0900

    🔥 del : 미사용 코드 정리

--- source/app.js
+++ source/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import expressSession from "express-session";
 import dotEnv from "dotenv";
-import expressMySQLSession from "express-mysql-session";
 import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import AccountsRouter from "./routes/accounts.router.js";
 import CharactersRouter from "./routes/characters.router.js";
@@ -15,30 +13,8 @@ dotEnv.config();
 const app = express();
 const PORT = 3000;
 
-/* const MySQLStore = expressMySQLSession(expressSession);
-const sessionStore = new MySQLStore({
-	user: process.env.DATABASE_USERNAME,
-	password: process.env.DATABASE_PASSWORD,
-	host: process.env.DATABASE_HOST,
-	port: process.env.DATABASE_PORT,
-	database: process.env.DATABASE_NAME,
-	expiration: 1000 * 60 * 60 * 1,
-	createDatabaseTable: true,
-}); */
-
 app.use(express.json());
 app.use(cookieParser());
-/* app.use(expressSession(
-	{
-		secret: process.env.SESSION_SECRET_KEY,
-		resave: false,
-		saveUninitialized: false,
-		store: sessionStore,
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 1,
-		},
-	}
-)); */
 app.use('/api', [AccountsRouter, CharactersRouter, ItemsRouter, StoreRouter, InventoryRouter]);
 app.use(errorHandlingMiddleware);
 
